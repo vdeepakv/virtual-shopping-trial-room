9d2de8d4b46eea12bc771927de4f30a4e27d7f4b
Deepak Velusamy <62099193+vdeepakv@users.noreply.github.com>
2023-10-08 14:32:31 +0530

    Add files via upload

---
+++ script.py
@@ -0,0 +1,7 @@
+from flask import Flask
+app = Flask(__name__) #creating the Flask class object
+@app.route('/')
+def home():
+    return "hello, this is our first flask website";
+if __name__ =='__main__':
+    app.run(debug = True)  
